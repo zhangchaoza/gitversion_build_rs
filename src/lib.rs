9a71ae5a5559a44a951e0a13a08bb98c50ff3c5e
zhangchao <zackability@hotmail.com>
2024-08-13 17:00:09 +0800

    Merge branch 'develop'

--- src/lib.rs
+++ src/lib.rs
@@ -34,73 +34,33 @@ pub fn inject_version() -> Result<(), String> {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
 pub struct GitVersion {
-    #[serde(rename = "Major")]
-    pub major: i64,
-    #[serde(rename = "Minor")]
-    pub minor: i64,
-    #[serde(rename = "Patch")]
-    pub patch: i64,
-    #[serde(rename = "PreReleaseTag")]
-    pub pre_release_tag: String,
-    #[serde(rename = "PreReleaseTagWithDash")]
-    pub pre_release_tag_with_dash: String,
-    #[serde(rename = "PreReleaseLabel")]
-    pub pre_release_label: String,
-    #[serde(rename = "PreReleaseLabelWithDash")]
-    pub pre_release_label_with_dash: String,
-    #[serde(rename = "PreReleaseNumber")]
-    pub pre_release_number: Option<serde_json::Value>,
-    #[serde(rename = "WeightedPreReleaseNumber")]
-    pub weighted_pre_release_number: i64,
-    #[serde(rename = "BuildMetaData")]
-    pub build_meta_data: Option<serde_json::Value>,
-    #[serde(rename = "BuildMetaDataPadded")]
-    pub build_meta_data_padded: String,
-    #[serde(rename = "FullBuildMetaData")]
-    pub full_build_meta_data: String,
-    #[serde(rename = "MajorMinorPatch")]
-    pub major_minor_patch: String,
-    #[serde(rename = "SemVer")]
-    pub sem_ver: String,
-    #[serde(rename = "LegacySemVer")]
-    pub legacy_sem_ver: String,
-    #[serde(rename = "LegacySemVerPadded")]
-    pub legacy_sem_ver_padded: String,
-    #[serde(rename = "AssemblySemVer")]
-    pub assembly_sem_ver: String,
-    #[serde(rename = "AssemblySemFileVer")]
     pub assembly_sem_file_ver: String,
-    #[serde(rename = "FullSemVer")]
-    pub full_sem_ver: String,
-    #[serde(rename = "InformationalVersion")]
-    pub informational_version: String,
-    #[serde(rename = "BranchName")]
+    pub assembly_sem_ver: String,
     pub branch_name: String,
-    #[serde(rename = "EscapedBranchName")]
-    pub escaped_branch_name: String,
-    #[serde(rename = "Sha")]
-    pub sha: String,
-    #[serde(rename = "ShortSha")]
-    pub short_sha: String,
-    #[serde(rename = "NuGetVersionV2")]
-    pub nu_get_version_v2: String,
-    #[serde(rename = "NuGetVersion")]
-    pub nu_get_version: String,
-    #[serde(rename = "NuGetPreReleaseTagV2")]
-    pub nu_get_pre_release_tag_v2: String,
-    #[serde(rename = "NuGetPreReleaseTag")]
-    pub nu_get_pre_release_tag: String,
-    #[serde(rename = "VersionSourceSha")]
-    pub version_source_sha: String,
-    #[serde(rename = "CommitsSinceVersionSource")]
-    pub commits_since_version_source: i64,
-    #[serde(rename = "CommitsSinceVersionSourcePadded")]
-    pub commits_since_version_source_padded: String,
-    #[serde(rename = "UncommittedChanges")]
-    pub uncommitted_changes: i64,
-    #[serde(rename = "CommitDate")]
+    pub build_meta_data: Option<serde_json::Value>,
     pub commit_date: String,
+    pub commits_since_version_source: i64,
+    pub escaped_branch_name: String,
+    pub full_build_meta_data: String,
+    pub full_sem_ver: String,
+    pub informational_version: String,
+    pub major: i64,
+    pub major_minor_patch: String,
+    pub minor: i64,
+    pub patch: i64,
+    pub pre_release_label: String,
+    pub pre_release_label_with_dash: String,
+    pub pre_release_number: i64,
+    pub pre_release_tag: String,
+    pub pre_release_tag_with_dash: String,
+    pub sem_ver: String,
+    pub sha: String,
+    pub short_sha: String,
+    pub uncommitted_changes: i64,
+    pub version_source_sha: String,
+    pub weighted_pre_release_number: i64,
 }
 
 fn get_git_version() -> String {
